217f92e67eaa07f42a02aeddbfdda5041d1f4155
U-Chive-PC\Chive <chivequiachon@gmail.com>
2018-01-29 22:03:26 +0800

    Added templates and sample welcome page

--- portal/views.py
+++ portal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def welcome_page(request):
-  return HttpResponse("This should serve as the welcome page.")
+  return render(request, 'pages/welcome_page.html')
--- portal46/settings.py
+++ portal46/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = [os.environ['CURRENT_HOST']]
 # Application definition
 
 INSTALLED_APPS = [
+    'portal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
