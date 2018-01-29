75e92f71dcf08da5889efb631499167ab5c66ec6
U-Chive-PC\Chive <chivequiachon@gmail.com>
2018-01-29 21:32:20 +0800

    Added urls and views for welcome page

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
-# Create your views here.
+def welcome_page(request):
+  return HttpResponse("This should serve as the welcome page.")
