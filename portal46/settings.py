6f9afb19d6151f5a5aa8e7ba8eb7a7f1281be61d
Chive Quiachon <chivequiachon@gmail.com>
2018-01-31 00:17:12 +0800

    fix for resource not found

--- portal46/settings.py
+++ portal46/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, 'templates'),
-    # Add to this list all the locations containing your static files 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
 )
