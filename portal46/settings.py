0e62828727ac58f5fc4882dd0adef07e449b9011
Chive Quiachon <chivequiachon@gmail.com>
2018-01-31 00:11:02 +0800

    Fix for resource not found

--- portal46/settings.py
+++ portal46/settings.py
@@ -129,4 +129,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, 'templates'),
+    # Add to this list all the locations containing your static files 
+)
