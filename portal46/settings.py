1f5f31d61a48e4969001033dd82a02d3843c24c4
Chive Quiachon <chivequiachon@gmail.com>
2018-01-31 17:53:30 +0800

    Update settings.py

--- portal46/settings.py
+++ portal46/settings.py
@@ -127,5 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
