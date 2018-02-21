de9cbb558b6f9de7ddf4f606b559b19409bce55b
U-Chive-PC\Chive <chivequiachon@gmail.com>
2018-02-21 23:35:34 +0800

    Added model for credential

--- portal/models.py
+++ portal/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.conf import settings
+
+from portal import endecoder
+
 
 class SubtitleFile(models.Model):
     subtitle_name = models.CharField(max_length=100)
@@ -9,3 +13,21 @@ class SubtitleFile(models.Model):
 
     def __str__(self):
         return self.subtitle_name
+
+
+class Credential(models.Model):
+    username = models.CharField(max_length=500)
+    password = models.CharField(max_length=500)
+    forum = models.CharField(max_length=30)
+    
+    def save(self, *args, **kwargs):
+        self.username = endecoder.encode(self.username)
+        self.password = endecoder.encode(self.password)
+    
+        super(Credential, self).save(*args, **kwargs)
+    
+    def publish(self):
+        self.save()
+        
+    def __str__(self):
+        return self.username
