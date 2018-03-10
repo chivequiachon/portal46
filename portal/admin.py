6da84572e4e42689d448b6cac0bcb02a0e581ea8
Chive Quiachon <chivequiachon@gmail.com>
2018-03-10 15:33:21 +0800

    Added watched fb pages to db

--- portal/admin.py
+++ portal/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SubtitleFile, Credential
+from .models import SubtitleFile, Credential, FbPage
 
 admin.site.register(SubtitleFile)
 admin.site.register(Credential)
+admin.site.register(FbPage)
